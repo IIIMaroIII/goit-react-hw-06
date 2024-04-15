11e304a279e36002f04a21ecb2a94872783fe6ad
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-04-14 23:04:08 -0400

    updated selectors.js

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,20 +2,24 @@ import css from "./contactList.module.css";
 import Contact from "./Contact/Contact";
 import { useSelector } from "react-redux";
 import { useMemo } from "react";
-import { selectContacts, selectNameFilter } from "../../redux/selectors";
+import { selectContacts, selectFilteredContacts } from "../../redux/selectors";
 
 const ContactList = () => {
   const items = useSelector(selectContacts);
-  const filterName = useSelector(selectNameFilter);
-  const filteredContacts = useMemo(
-    () =>
-      items.filter((item) =>
-        item.name.toLowerCase().includes(filterName.toLowerCase())
-      ),
-    [filterName, items]
-  );
-  const isFilteredContactsEmpty = filteredContacts.length === 0;
+  // const filterName = useSelector(selectNameFilter);
+  const res = useSelector(selectFilteredContacts);
+  const filteredContacts = useMemo(() => {
+    return res;
+  }, [res]);
+  // const filteredContacts = useMemo(
+  //   () =>
+  //     items.filter((item) =>
+  //       item.name.toLowerCase().includes(filterName.toLowerCase())
+  //     ),
+  //   [filterName, items]
+  // );
 
+  const isFilteredContactsEmpty = filteredContacts.length === 0;
   const isContactsEmpty = items.length === 0;
 
   return (
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,13 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+
 export const selectContacts = (state) => state.contacts.items;
 export const selectNameFilter = (state) => state.filters.name;
+
+export const selectFilteredContacts = (state) => {
+  const items = selectContacts(state);
+  const filterName = selectNameFilter(state);
+  return items.filter((item) =>
+    item.name.toLowerCase().includes(filterName.toLowerCase())
+  );
+};
